3c0512a4bd64ee0ce9b866bb74c5aa1c24499539
Michael-c0de <1679556937@qq.com>
2024-10-16 14:34:42 +0800

    优化任务队列

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
 import sys
 import threading
 from collections import deque
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QTableWidget, QTableWidgetItem, QHeaderView
 from PyQt5.QtCore import pyqtSignal, QObject
 import libpcap as pcap
 import ctypes as ct
@@ -9,15 +9,20 @@ from scapy.all import Ether
 from device_mm import PcapDeviceManager
 import sched
 import time
+from queue import Queue
+from PyQt5.QtCore import pyqtSlot, QTimer
+
 
 class PacketCaptureThread(threading.Thread):
     """抓包线程"""
-    def __init__(self, packet_queue:deque, dev, bpf_exp='udp'):
+    def __init__(self, packet_queue:deque, dev, stop_event:threading.Event, bpf_exp='udp', max_count=1000):
         super().__init__()
         self.packet_queue = packet_queue
         self.dev = dev
+        self.stop_event = stop_event
         self.bpf_exp = bpf_exp
         self.handle = None
+        self.max_count = max_count
         self.setDaemon(True)
 
     def set_bpf_filter(self):
@@ -43,24 +48,29 @@ class PacketCaptureThread(threading.Thread):
         PCAP_HANDLER = ct.CFUNCTYPE(None, ct.POINTER(ct.c_ubyte), ct.POINTER(pcap.pkthdr), ct.POINTER(ct.c_ubyte))
         
         # 时间检测
-        result = pcap.loop(self.handle, 0, PCAP_HANDLER(self.packet_handler), None)  # 逐包处理
+        result = pcap.loop(self.handle, self.max_count, PCAP_HANDLER(self.packet_handler), None)  # 逐包处理
         if result != pcap.PCAP_ERROR_BREAK :
             print(f"pcap loop exit: {result}")
-        pcap.close(self.handle)
-        print("PacketCaptureThread end")
+        self.stop()
     
     def packet_handler(self, user, pkthdr, packet):
         """处理抓到的包，放入队列"""
         raw_data = bytes(packet[:pkthdr.contents.caplen])
         if len(self.packet_queue) < self.packet_queue.maxlen:
             self.packet_queue.append(raw_data)  # 将原始数据包放入队列
+    
     def stop(self):
         if self.handle:
             pcap.breakloop(self.handle)
+            pcap.close(self.handle)
+            self.handle = None
+            self.stop_event.set()
+            print("PacketCaptureThread end")
+            
 
 class PacketAnalysisThread(threading.Thread):
     """数据分析线程"""
-    def __init__(self, packet_queue:deque, result_queue:deque, stop_event):
+    def __init__(self, packet_queue:deque, result_queue:Queue, stop_event:threading.Event):
         super().__init__()
         self.packet_queue = packet_queue
         self.result_queue = result_queue
@@ -69,7 +79,7 @@ class PacketAnalysisThread(threading.Thread):
 
     def run(self):
         """线程运行函数，处理数据包并进行分析"""
-        while not self.stop_event.is_set():
+        while not self.stop_event.is_set() or len(self.packet_queue)!=0:
             try:
                 packet_data = self.packet_queue.pop()
                 # 解析数据包
@@ -78,13 +88,15 @@ class PacketAnalysisThread(threading.Thread):
                 result = f"Packet Summary: {frame.summary()}"
                 print(f"#{self.count}, {result}")
                 self.count+=1
-                self.result_queue.append(result)  # 将分析结果放入结果队列
+                self.result_queue.put(result)  # 将分析结果放入结果队列
             except Exception as e:
                 continue
         self.stop()
+        print("PacketAnalysisThread end")
+
     def stop(self):
         """停止分析"""
-        print("PacketAnalysisThread end")
+        self.stop_event.set()
 
 class SignalManager(QObject):
     """信号管理类，用于子线程和主线程通信"""
@@ -92,45 +104,45 @@ class SignalManager(QObject):
 
 class MainWindow(QMainWindow):
     """Qt主窗口，用于数据呈现和用户交互"""
-    def __init__(self, result_queue:deque, signal_manager, stop_event):
+    def __init__(self, result_queue:Queue):
         super().__init__()
         self.result_queue = result_queue
-        self.signal_manager = signal_manager
-        self.stop_event = stop_event
         # 初始化UI
         self.setWindowTitle("Packet Sniffer")
-        self.setGeometry(100, 100, 600, 400)
-        layout = QVBoxLayout()
+        self.setGeometry(100, 100, 800, 400)
 
-        self.label = QLabel("Packet Data:")
-        layout.addWidget(self.label)
+        # 设置主部件为一个表格，用于显示抓包信息
+        self.table = QTableWidget(self)
+        self.table.setColumnCount(1)
+        self.table.setHorizontalHeaderLabels(["INFO"])
+        # 设置列宽自适应内容
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.table)
 
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
-        # 连接信号槽，更新UI
-        self.signal_manager.result_signal.connect(self.update_ui)
-        self.scheduler = sched.scheduler(time.time, time.sleep)
-        
-        self.scheduler.enter(1, 1, self.poll_results)
-        timer = threading.Thread(target=lambda:self.scheduler.run())
-        timer.start()
-    
-    def update_ui(self, packet_summary):
-        """更新UI，显示分析结果"""
-        self.label.setText(packet_summary)
+        # 定时器定时刷新抓包数据
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_table)
+        self.timer.start(1000)  # 每秒刷新一次
 
-    def poll_results(self):
-        if not self.stop_event.is_set():
-            try:
-                while len(self.result_queue)!=0:
-                    result = self.result_queue.pop()
-                    self.signal_manager.result_signal.emit(result)
-            except Exception as e:
-                print(f"poll_results err {e}")
-                pass
-            self.scheduler.enter(1, 1, self.poll_results)
+
+    @pyqtSlot()
+    def update_table(self):
+        """定时刷新抓包数据"""
+        while not self.result_queue.empty():
+            result = self.result_queue.get()
+            row_count = self.table.rowCount()
+            self.table.insertRow(row_count)
+            self.table.setItem(row_count, 0, QTableWidgetItem(result))
+
+
+
 
 def select_device():
     """选择设备"""
@@ -151,19 +163,16 @@ def main():
     dev = select_device()
     # 创建队列和线程停止事件
     packet_queue = deque(maxlen=1000000)
-    result_queue = deque(maxlen=1000000)
+    result_queue = Queue(maxsize=1000000)
     stop_event = threading.Event()
-
-    # 创建信号管理器
-    signal_manager = SignalManager()
 
     # 初始化 Qt 应用
     app = QApplication(sys.argv)
-    main_window = MainWindow(result_queue, signal_manager, stop_event)
+    main_window = MainWindow(result_queue)
     main_window.show()
 
     # 创建抓包线程和数据分析线程
-    capture_thread = PacketCaptureThread(packet_queue, dev, "udp")
+    capture_thread = PacketCaptureThread(packet_queue, dev, stop_event, "udp")
     analysis_thread = PacketAnalysisThread(packet_queue, result_queue, stop_event)
 
     # 启动线程
@@ -172,11 +181,11 @@ def main():
 
     # 启动应用
     app.exec_()
-    capture_thread.stop()
-    analysis_thread.stop()
+
 
     # 停止线程
-    stop_event.set()
+    capture_thread.stop()
+    analysis_thread.stop()
     capture_thread.join()
     analysis_thread.join()
 
